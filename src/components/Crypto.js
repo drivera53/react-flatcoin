96fe19ca4f77f052f315d656a8d01f96a31d037f
Danny Rivera <samboard@hotmail.com>
2021-06-20 09:28:39 -0400

    Adding style to crypto list.

--- src/components/Crypto.js
+++ src/components/Crypto.js
@@ -1,12 +1,22 @@
-import React, { Component } from 'react' 
+import React, { Component } from 'react'
+import './Crypto.css' 
 
 export class Crypto extends React.Component {
     render() {
-        const {name, image, id} = this.props.crypto
+        const {name, image, id, current_price, price_change_percentage_24h, symbol} = this.props.crypto
         return (
-            <div id="crypto">
-                <h3>{name}</h3>
-                <img key={id} src={image} alt={name} />
+            <div id="crypto" className="row">
+                <div>
+                    <img key={id} src={image} alt={name} height={50}/>
+                </div>
+                <div className="row__intro">
+                    <h1>{name}</h1>
+                    <p>{symbol} - 200 Coins</p>
+                </div>
+                <div className="row__numbers">
+                    <p className="row__price">${current_price}</p>
+                    <p className="row_percentage">{price_change_percentage_24h}%</p>
+                </div> 
             </div>
         ) 
     }
--- src/components/CryptoList.js
+++ src/components/CryptoList.js
@@ -1,13 +1,26 @@
 import React from 'react'
-import Crypto from './Crypto' 
+import Crypto from './Crypto'
+import './CryptoList.css'
 
 class CryptoList extends React.Component {
 
     render() {
         const cryptos = this.props.cryptoData.map(c => <Crypto crypto={c} />)
         return (
-            <div>
-                {cryptos}
+            <div className="cryptoList">
+                <div className="cryptoList__container">
+                    <div className="crypto__header">
+                        <p>Top 20 Cryptocurrencies</p>
+                    </div>
+                    <div className="crypto__content">
+                        <div className="crypto__rows">
+                        {cryptos}
+                        </div>
+                    </div>
+                    <div className="crypto__header">
+                        <p>My List</p>
+                    </div>
+                </div>
             </div>
         )
     }
